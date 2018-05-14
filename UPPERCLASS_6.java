8130748de91b3c77c52ce216644f7291f34a8558
mdnawab007 <39163657+mdnawab007@users.noreply.github.com>
2018-05-14 19:29:17 +0530

    Add files via upload

---
+++ UPPERCLASS_6.java
@@ -0,0 +1,37 @@
+/*
+6.Write a program to input character from user and check whether character is uppercase or lowercase alphabet using if else. 
+Example
+Input
+Input character: C
+Output
+'C' is uppercase alphabet
+Input
+Input character: s
+Output
+'s' is lowercase alphabet
+*/
+class UPPERCLASS_6
+{
+    public static void main()
+    {
+        upper('C');
+        upper('s');
+    }
+    public static void upper(char m)
+    {
+        if(m>='A' && m<='Z')
+        {
+            System.out.println(m + " is UPPERCASE");
+        }
+        else 
+        {
+            System.out.println(m + " is LOWERCASE");
+        }
+    }
+}
+/*  
+  OUTPUT
+ C is UPPERCASE
+s is LOWERCASE
+*/       
+        
---
+++ VOWELCONSONANT_4.java
@@ -0,0 +1,37 @@
+/*
+4.Write a program to check whether an alphabet is vowel or consonant using if else. 
+Example
+Input
+Input character: a
+Output
+'a' is vowel
+Input
+Input character: s
+Output
+'s' is not vowel
+*/
+class VOWELCONSONANT_4
+{
+    public static void main()
+    {
+        vowel('a');
+        vowel('s');
+    }
+    public static void vowel(char m)
+    {
+        if(m=='a' || m=='e' || m=='i' || m=='o' || m=='u')
+        {
+        System.out.println(m  + " is vowel");
+    }
+    else
+    {
+        System.out.println(m  + " is not vowel");
+    }
+    }
+}
+/*
+ * OUTPUT
+ * a is vowel
+s is not vowel
+
+ */
